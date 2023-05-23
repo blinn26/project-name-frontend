91c134604ee79e16911562010718e73cf2b3f084
createdbybenlinn <benlinn26@gmail.com>
2023-05-23 12:36:43 -0700

    footer stuff

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,11 @@ const Footer = () => {
   return (
     <footer className='footer'>
       <p className='footer__text'>© 2023 Ben's News Page</p>
-      <p className='footer__links'>Ben's Global Warming Crisis</p>
+      <p className='footer__links'>
+        <a href='https://www.example.com' target='_blank' rel='noopener noreferrer'>
+          Ben's Global Warming Crisis
+        </a>
+      </p>
     </footer>
   );
 };
