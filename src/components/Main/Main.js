42f988cb62406be82b3b75711ca09b1eaf4c7fec
createdbybenlinn <benlinn26@gmail.com>
2023-05-23 13:20:37 -0700

    changed for saving purpose

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchNews } from '../../utils/ThirdPartyApi';
+import NewsCardList from '../NewsCardList/NewsCardList';
 import './Main.css';
 
 const Main = () => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    fetchNews()
+      .then((data) => {
+        setNews(data.articles);
+      })
+      .catch((error) => {
+        console.error('Error fetching data: ', error);
+      });
+  }, []);
+
   return (
     <div className='main'>
       <h1 className='main__title'>Welcome to our News Site</h1>
-      {/* Additional content goes here */}
+      <NewsCardList news={news} />
     </div>
   );
 };
--- src/utils/ThirdPartyApi.js
+++ src/utils/ThirdPartyApi.js
@@ -1,8 +1,10 @@
-// ThirdPartyApi.js
 const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
 const BASE_URL = 'https://newsapi.org/v2/top-headlines';
 
 export const fetchNews = async () => {
+  // Console log here for testing purposes
+  console.log('API key is:', API_KEY);
+
   const response = await fetch(`${BASE_URL}?country=us&apiKey=${API_KEY}`);
   if (!response.ok) {
     throw new Error('Network response was not ok');
