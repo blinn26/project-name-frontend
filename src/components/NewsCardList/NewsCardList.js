45ad642ff167deef33cae54c20cc061c8e4c9996
createdbybenlinn <benlinn26@gmail.com>
2023-05-23 13:46:14 -0700

    saved news card

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './NewsCardList.css';
 import NewsCard from '../NewsCard/NewsCard';
+import { fetchNews } from '../../utils/ThirdPartyApi'; // Update this path as necessary
 
-const NewsCardList = ({ news }) => {
+const NewsCardList = () => {
+  const [news, setNews] = useState(null);
+
+  useEffect(() => {
+    const loadNews = async () => {
+      const data = await fetchNews();
+      setNews(data.articles);
+    };
+
+    loadNews();
+  }, []);
+
   return (
     <div className='news-card__list'>
       <h2 className='news-card__title'>News</h2>
-      {news && news.length > 0 ? (
-        news.map((item, index) => <NewsCard key={index} newsItem={item} />)
-      ) : (
-        <p>Loading...</p>
-      )}
+      {news ? news.map((item, index) => <NewsCard key={index} newsItem={item} />) : <p>Loading...</p>}
     </div>
   );
 };
