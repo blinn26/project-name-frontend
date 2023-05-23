3e767799495f54f25c271c0a34a1445699512728
createdbybenlinn <benlinn26@gmail.com>
2023-05-23 13:34:07 -0700

    push change

--- src/utils/ThirdPartyApi.js
+++ src/utils/ThirdPartyApi.js
@@ -1,14 +1,15 @@
-const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
-const BASE_URL = 'https://newsapi.org/v2/top-headlines';
+const apiKey = process.env.REACT_APP_NEWS_API_KEY;
 
 export const fetchNews = async () => {
-  // Console log here for testing purposes
-  console.log('API key is:', API_KEY);
+  console.log('API key is:', apiKey);
 
-  const response = await fetch(`${BASE_URL}?country=us&apiKey=${API_KEY}`);
+  const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`);
+
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
+
   const data = await response.json();
-  return data;
+  console.log(data);
+  return data.articles;
 };
