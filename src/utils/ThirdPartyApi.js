e966a643e6a83cf026bf634b0fe08770a4c3736b
createdbybenlinn <benlinn26@gmail.com>
2023-05-23 21:16:54 -0700

    save code

--- src/utils/ThirdPartyApi.js
+++ src/utils/ThirdPartyApi.js
@@ -3,13 +3,17 @@ const apiKey = process.env.REACT_APP_NEWS_API_KEY;
 export const fetchNews = async () => {
   console.log('API key is:', apiKey);
 
-  const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`);
+  // Comment out the fetch line to disable the API request.
+  // const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`);
 
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
+  // if (!response.ok) {
+  //   throw new Error('Network response was not ok');
+  // }
 
-  const data = await response.json();
-  console.log(data);
-  return data.articles;
+  // const data = await response.json();
+  // console.log(data);
+  // return data.articles;
+
+  // Instead, you can return an empty array or a mock data
+  return [];
 };
