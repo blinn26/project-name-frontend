74125cee7c57707455d4210a48b7cbb3b143fff9
createdbybenlinn <benlinn26@gmail.com>
2023-05-23 21:44:12 -0700

    commmmit changes

--- src/components/Preloader/Preloader.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Preloader.css';
-
-const Preloader = () => {
-  return <div className='preloader'>{/* Loading indication goes here */}</div>;
-};
-
-export default Preloader;
