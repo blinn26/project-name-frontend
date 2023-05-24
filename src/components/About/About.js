a522ec89636064431c0d6df6363de42df939c39d
createdbybenlinn <benlinn26@gmail.com>
2023-05-24 10:16:31 -0700

    edit changes before frustration

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,8 +5,8 @@ import benImage from '../images/BenImage.jpeg';
 const About = () => {
   return (
     <section className='about'>
-      <h2 className='about__title'>About the Author</h2>
-      <p className='about__text'>I am Ben, a web developer passionate about news.</p>
+      <h2 className='about__title'></h2>
+      <p className='about__text'></p>
       {<img className='about__image' src={benImage} alt='benselfie' />}
     </section>
   );
