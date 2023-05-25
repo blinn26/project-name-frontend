fb97797a301466cdaa8ace3e701a4a54dc75ff85
createdbybenlinn <benlinn26@gmail.com>
2023-05-24 22:20:24 -0700

    Bedtime night night

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Main.css';
 import blackonWhite from '../images/phonepic.png';
+import About from '../About/About';
 /* import Preloader from '../Preloader/Preloader'; */
 
 const Main = () => {
   return (
     <div className='main' style={{ backgroundImage: `url(${blackonWhite})` }}>
       <h1 className='main__title'></h1>
+      <h2 className='main__subtitle'></h2>
+      <About />
       {/*  {<Preloader />} */}
     </div>
   );
--- src/components/SignInandUpModal/SignInandUpModal.js
+++ src/components/SignInandUpModal/SignInandUpModal.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import ModalWithForm from '../ModalWithForm/ModalWithForm';
 
-function SignInandUpModal({ isOpen, onClose, onLogin, switchToRegister }) {
+function SignInandUpModal({ isOpen, onClose }) {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isSignUp, setIsSignUp] = useState(true); // initial state is sign up
   const [isValidEmail, setIsValidEmail] = useState(true);
+  const [isValidUsername, setIsValidUsername] = useState(true);
 
   useEffect(() => {
     console.log(`Modal open state: ${isOpen}`);
@@ -17,20 +18,23 @@ function SignInandUpModal({ isOpen, onClose, onLogin, switchToRegister }) {
     return re.test(email);
   };
 
+  const validateUsername = (username) => {
+    return username.length >= 2 && username.length <= 30; // adjust as needed
+  };
+
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
     setIsValidEmail(validateEmail(e.target.value));
   };
 
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+    setIsValidUsername(validateUsername(e.target.value));
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     // Validation for confirming password can be done here
-  };
-
-  const handleCloseOnOverlayClick = (event) => {
-    if (event.target === event.currentTarget) {
-      onClose();
-    }
   };
 
   const toggleForm = () => {
@@ -41,42 +45,46 @@ function SignInandUpModal({ isOpen, onClose, onLogin, switchToRegister }) {
   };
 
   return (
-    <ModalWithForm isOpen={isOpen} onClose={onClose} onSubmit={handleSubmit} buttonText='Sign In' isValid>
+    <ModalWithForm
+      isOpen={isOpen}
+      onClose={onClose}
+      onSubmit={handleSubmit}
+      buttonText={isSignUp ? 'Sign Up' : 'Sign In'}
+      isValid>
       <h2 className='modal__title'>{isSignUp ? 'Sign Up' : 'Sign In'}</h2>
+      {isSignUp && (
+        <input
+          className={`modal__input ${!isValidUsername ? 'invalid' : ''}`}
+          type='text'
+          value={username}
+          onChange={handleUsernameChange}
+          placeholder='Username'
+          required
+        />
+      )}
       <input
-        className={`modal__input ${!isValidEmail ? 'input_invalid' : ''}`}
+        className={`modal__input ${!isValidEmail ? 'invalid' : ''}`}
         type='email'
         value={email}
         onChange={handleEmailChange}
         placeholder='Email'
+        required
       />
       {!isValidEmail && <span className='input_error'>Invalid email address</span>}
       <input
-        className='modal__input'
+        className={`modal__input ${!isValidUsername ? 'invalid' : ''}`}
         type='password'
         value={password}
         onChange={(e) => setPassword(e.target.value)}
         placeholder='Password'
+        required
       />
-      {isSignUp && (
-        <input
-          className='modal__input'
-          type='text'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder='Username'
-        />
-      )}
-      <button className='modal__button-submit' type='submit'>
-        {isSignUp ? 'Sign Up' : 'Sign In'}
-      </button>
       <p>
         <span className='or-text'>or</span>{' '}
         <button className='toggle-form-button' onClick={toggleForm}>
           {isSignUp ? 'Sign In' : 'Sign Up'}
         </button>
       </p>
-      {/*  <button className='modal__button-close' type='button' onClick={onClose}></button> */}
     </ModalWithForm>
   );
 }
