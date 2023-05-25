8131c48425bee60859aa27dd92764003033a18f7
createdbybenlinn <benlinn26@gmail.com>
2023-05-25 09:49:17 -0700

    good start to the morning

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import './Footer.css';
+import github from '../images/github.png';
+import linkedIn from '../images/linkedin.png';
+import twitter from '../images/twitter.png';
+import practicum from '../images/Practicum.png';
 
 const Footer = () => {
+  const handleNavigation = (url) => {
+    window.open(url, '_blank');
+  };
+
   return (
     <footer className='footer'>
-      <p className='footer__text'>© 2023 Ben's News Page</p>
-      <p className='footer__links'>
-        <a href='https://www.example.com' target='_blank' rel='noopener noreferrer'>
-          Ben's Global Warming Crisis
-        </a>
-      </p>
+      <div className='footer__copyright'>© 2023 Powered by Ben Linn's Brain</div>
+      <div className='footer__links'>
+        <button onClick={() => handleNavigation('https://twitter.com/createdbyben26')} className='footer__button'>
+          <img alt='Twitter logo' src={twitter} className='footer__icon' />
+        </button>
+        <button
+          onClick={() => handleNavigation('https://www.linkedin.com/in/ben-linn-coding4l/')}
+          className='footer__button'>
+          <img alt='LinkedIn logo' src={linkedIn} className='footer__icon' />
+        </button>
+        <button onClick={() => handleNavigation('https://github.com/blinn26')} className='footer__button'>
+          <img alt='Github logo' src={github} className='footer__icon' />
+        </button>
+        <button onClick={() => handleNavigation('https://practicum.com')} className='footer__button'>
+          <img alt='Practicum logo' src={practicum} className='footer__practicum-icon' />
+        </button>
+        <button onClick={() => handleNavigation('https://newsapi.org')} className='footer__button footer__email'>
+          newsapi.org
+        </button>
+      </div>
     </footer>
   );
 };
