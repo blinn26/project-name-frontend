307aa46ae7aa54e3ae20ea25bd842aff828c0bf1
createdbybenlinn <benlinn26@gmail.com>
2023-05-31 16:24:30 -0700

    going home

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,31 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
 import logo from '../images/NewsExplorer.png';
 import homeImage from '../images/HomeWhite.png';
 
-const Navigation = ({ handleModalOpen, isLoggedIn, handleLogOut }) => {
-  const [activeTab, setActiveTab] = useState('home');
-
-  useEffect(() => {
-    setActiveTab(isLoggedIn ? 'article' : 'home');
-  }, [isLoggedIn]);
-
+const Navigation = ({ isLoggedIn, handleLogOut, handleModalOpen }) => {
   return (
     <nav className='navigation' data-theme={isLoggedIn ? 'light' : 'dark'}>
       <img className='navigation__logo' src={logo} alt='Logo' />
-      <ul className='navigation__list'>
-        <li className={`navigation__item ${activeTab === 'home' ? 'active' : ''}`}>
-          <Link to='/'>
-            <img src={homeImage} alt='Home' className='navigation__home-image' />
-          </Link>
-        </li>
-        {isLoggedIn && (
-          <li className={`navigation__item ${activeTab === 'article' ? 'active' : ''}`}>
-            <Link to='/saved-news'>Saved articles</Link>
-          </li>
-        )}
-      </ul>
+
+      <Link to='/'>
+        <img src={homeImage} alt='Home' className='navigation__home-image' />
+      </Link>
+
+      {isLoggedIn && <Link to='/saved-news'>Saved articles</Link>}
 
       {isLoggedIn ? (
         <div className='navigation__user'>
