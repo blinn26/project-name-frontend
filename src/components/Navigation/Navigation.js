01c57c428bb270094ceaf963d1efdaa0143b7c1e
createdbybenlinn <benlinn26@gmail.com>
2023-06-01 00:17:54 -0700

    GOODNIGHT CODE

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,33 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Navigation.css';
 import logo from '../images/NewsExplorer.png';
+import UserLogoutImage from '../images/UserLogOut.png';
+import homeImg from '../images/Home.png';
 
 const Navigation = ({ isLoggedIn, handleLogOut, handleModalOpen }) => {
+  const location = useLocation();
   const textColorClass = isLoggedIn ? 'text-black' : 'text-white';
+
   return (
     <nav className='navigation' data-theme={isLoggedIn ? 'light' : 'dark'}>
       <img className='navigation__logo' src={logo} alt='Logo' />
 
       <Link to='/' className={`navigation__home-link ${textColorClass}`}>
-        <p className='navigation__home-link-text'>Home</p>
-        {!isLoggedIn && <div className='navigation__home-link-underline'></div>}
+        {!isLoggedIn ? (
+          <img className='navigation__home-link-image' src={homeImg} alt='Home' />
+        ) : (
+          <p className='navigation__home-link-text'>Home</p>
+        )}
+        {!isLoggedIn && location.pathname === '/' && <div className='navigation__home-link-underline'></div>}
       </Link>
 
       {isLoggedIn && (
         <Link to='/saved-news' className={`navigation__saved-news-link ${textColorClass}`}>
           Saved articles
-          <div className='navigation__saved-news-link-underline'></div>
+          {location.pathname === '/saved-news' && <div className='navigation__saved-news-link-underline'></div>}
         </Link>
       )}
 
       {isLoggedIn ? (
-        <div className='navigation__user'>
-          <p className='navigation__username'>Username</p>
-          <button onClick={handleLogOut} className='navigation__logout'>
-            Log Out
-          </button>
-        </div>
+        <button
+          className='navigation__user'
+          onClick={handleLogOut}
+          style={{ backgroundImage: `url(${UserLogoutImage})` }}
+        />
       ) : (
         <button className='navigation__button signIn' onClick={handleModalOpen}>
           Sign In
