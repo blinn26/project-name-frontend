b9ef29550efbf4ac91a284ddb699031e742713d4
createdbybenlinn <benlinn26@gmail.com>
2023-06-24 00:50:31 -0700

    foooter save

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,71 +9,76 @@ import home from '../../images/home.svg';
 import apiLogo from '../../images/NewsAPILogo.svg';
 
 const Footer = () => {
-  const handleNavigation = (url) => {
+  const handleNavigation = (url, event) => {
+    event.preventDefault();
     window.open(url, '_blank');
   };
 
   return (
     <footer className='footer'>
       <div className='footer__left'>
-        <p className='footer__copyright'>© 2023 SuperSite, Powered by News API Ben Linn</p>
+        <p className='footer__copyright'>©2023 Supersite, Powered by NewsAPI</p>
       </div>
       <nav className='footer__links'>
-        <Link
-          to='/'
-          className='footer__button footer__home-button'>
-          <img
-            alt='Home'
-            src={home}
-            className='footer__home-icon'
-          />
-        </Link>
-        <button
-          onClick={() => handleNavigation('https://twitter.com/createdbyben26')}
-          className='footer__button'>
-          <img
-            alt='Twitter logo'
-            src={twitter}
-            className='footer__icon-twitter'
-          />
-        </button>
+        <div className='footer__links-left'>
+          <Link
+            to='/'
+            className='footer__button footer__home-button'>
+            <img
+              alt='Home'
+              src={home}
+              className='footer__home-icon'
+            />
+          </Link>
+          <a
+            onClick={(event) => handleNavigation('https://practicum.com', event)}
+            className='footer__button'>
+            <img
+              alt='tripleten logo'
+              src={tripleten}
+              className='footer__tripleten-icon'
+            />
+          </a>
+        </div>
+        <div className='footer__links-right'>
+          <a
+            onClick={(event) => handleNavigation('https://twitter.com/createdbyben26', event)}
+            className='footer__button'>
+            <img
+              alt='Twitter logo'
+              src={twitter}
+              className='footer__icon-twitter'
+            />
+          </a>
 
-        <button
-          onClick={() => handleNavigation('https://www.linkedin.com/in/ben-linn-coding4l/')}
-          className='footer__button'>
-          <img
-            alt='LinkedIn logo'
-            src={linkedIn}
-            className='footer__icon'
-          />
-        </button>
-        <button
-          onClick={() => handleNavigation('https://github.com/blinn26')}
-          className='footer__button'>
-          <img
-            alt='Github logo'
-            src={github}
-            className='footer__icon'
-          />
-        </button>
-        <button
-          onClick={() => handleNavigation('https://practicum.com')}
-          className='footer__button'>
-          <img
-            alt='tripleten logo'
-            src={tripleten}
-            className='footer__tripleten-icon'
-          />
-        </button>
-        <button
-          onClick={() => handleNavigation('https://newsapi.org')}
-          className='footer__button footer__api-button'>
-          <img
-            alt='nlogo'
-            src={apiLogo}
-            className='footer__api-icon'
-          />
-        </button>
+          <a
+            onClick={(event) => handleNavigation('https://www.linkedin.com/in/ben-linn-coding4l/', event)}
+            className='footer__button'>
+            <img
+              alt='LinkedIn logo'
+              src={linkedIn}
+              className='footer__icon'
+            />
+          </a>
+          <a
+            onClick={(event) => handleNavigation('https://github.com/blinn26', event)}
+            className='footer__button'>
+            <img
+              alt='Github logo'
+              src={github}
+              className='footer__icon'
+            />
+          </a>
+          <a
+            onClick={(event) => handleNavigation('https://newsapi.org', event)}
+            className='footer__button footer__api-button'>
+            <img
+              alt='nlogo'
+              src={apiLogo}
+              className='footer__api-icon'
+            />
+          </a>
+        </div>
       </nav>
     </footer>
   );
